0713b7792bae64c8c7993309f4bb754eb48f55e0
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2022-02-04 17:46:05 +0700

    get a movie and genre

--- cmd/api/movieHandler.go
+++ cmd/api/movieHandler.go
@@ -4,9 +4,6 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
-	"time"
-
-	"movies-app-backend/models"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -21,18 +18,7 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	movie := models.Movie{
-		ID:          id,
-		Title:       "some movie",
-		Description: "some desc",
-		Year:        2022,
-		ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
-		Runtime:     100,
-		Rating:      5,
-		MPAARating:  "PG-13",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
+	movie, err := app.models.DB.Get(id)
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
 	if err != nil {
---
+++ models/movies-db.go
@@ -0,0 +1,77 @@
+package models
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+type DBModel struct {
+	DB *sql.DB
+}
+
+// Get return one movie and error
+func (m *DBModel) Get(id int) (*Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `SELECT id, title, description, year, release_date, runtime, mpaa_rating, created_at, updated_at
+			  FROM movies
+			  WHERE id = $1`
+
+	row := m.DB.QueryRowContext(ctx, query, id)
+
+	var movie Movie
+
+	err := row.Scan(
+		&movie.ID,
+		&movie.Title,
+		&movie.Description,
+		&movie.Year,
+		&movie.ReleaseDate,
+		&movie.Runtime,
+		&movie.MPAARating,
+		&movie.CreatedAt,
+		&movie.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// get the genres
+	query = `SELECT 
+				mg.id, mg.movie_id, mg.genre_id, g.genre_name
+			 FROM
+			 	movies_genres mg
+				LEFT JOIN genres g
+				ON (g.id = mg.genre_id)
+			WHERE
+				mg.movie_id = $1`
+	rows, _ := m.DB.QueryContext(ctx, query, id)
+	defer rows.Close()
+
+	genres := make(map[int]string)
+
+	for rows.Next() {
+		var mg MovieGenre
+		err := rows.Scan(
+			&mg.ID,
+			&mg.MovieID,
+			&mg.GenreID,
+			&mg.Genre.GenreName,
+		)
+		if err != nil {
+			return nil, err
+		}
+		genres[mg.ID] = mg.Genre.GenreName
+	}
+
+	movie.MovieGenre = genres
+
+	return &movie, nil
+}
+
+// Get return all movies and error
+func (m *DBModel) All(id int) (*Movie, error) {
+	return nil, nil
+}
