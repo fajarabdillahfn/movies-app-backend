ecad190ef0161bc0095dfe6ff17ca46b7d4c4426
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2022-02-09 22:13:31 +0700

    fix format of jwt

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -58,5 +58,5 @@ func (app *application) signIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.writeJSON(w, http.StatusOK, jwtBytes, "response ")
+	app.writeJSON(w, http.StatusOK, string(jwtBytes), "response ")
 }
