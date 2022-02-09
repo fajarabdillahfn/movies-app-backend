45f57d26ff0c270d4349719c981a9b82808ded0a
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2022-02-09 22:12:19 +0700

    change route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,7 +11,7 @@ func (app *application) routes() http.Handler {
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
-	router.HandlerFunc(http.MethodPost, "/signin", app.signIn)
+	router.HandlerFunc(http.MethodPost, "/v1/signin", app.signIn)
 
 	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
