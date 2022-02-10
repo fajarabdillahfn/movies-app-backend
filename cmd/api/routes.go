77b8bd73ef9b31cc5635833bb78b42cb8c916083
fajarabdillahfn <fajar.abdillahfn@gmail.com>
2022-02-10 08:46:31 +0700

    securitize delete method

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,19 +20,29 @@ func (app *application) routes() http.Handler {
 
 	secure := alice.New(app.checkToken)
 
+	// Get API info
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
+	// Sign in method
 	router.HandlerFunc(http.MethodPost, "/v1/signin", app.signIn)
 
+	// Get a movie data
 	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
+	
+	// Get all movie data
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
+
+	// Get a list of movie by its genre
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMOviesByGenre)
 
+	// Get all genre data
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 
+	// Edit existing data
 	router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editMovie)))
 
-	router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
+	// Delete existing data
+	router.GET("/v1/admin/deletemovie/:id", app.wrap(secure.ThenFunc(app.deleteMovie)))
 	
 	return app.enableCORS(router)
 }
